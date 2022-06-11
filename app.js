108289a6d7cfb5b375529f6e01dab81d30ccdb30
Mohamad Alshekho <92674451+mohamadmarmara@users.noreply.github.com>
2022-06-11 11:48:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://Mm533487:Mm533487@cluster0.n5sev.mongodb.net/shop?retryWrites=true&w=majority';
+  'mongodb-url';
 
 const app = express();
 const store = new MongoDBStore({
